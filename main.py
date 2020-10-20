7e40be45d547bf70de0f090b2615a1cd5fbf0b06
ChrisEdBurt <chrisedburt@gmail.com>
2020-10-20 15:37:20 +0900

    Single use is okay, loop is not.

--- main.py
+++ main.py
@@ -1,80 +1,141 @@
+# coding=utf-8
+"""
+Main bot script file
+"""
 import imutils
 import pyautogui
 import cv2
 import numpy as np
 import os
-
-# try:
-#     os.remove("./screenshots/screenshot.jpg")
-#     os.remove("screenshot.jpg)")
-# except FileNotFoundError:
-#     print()
-
-# pyautogui.screenshot("./screenshots/screenshot.jpg")
-# pyautogui.screenshot("./screenshots/screenshot.jpg")
+from time import time, sleep
 
 font = cv2.FONT_HERSHEY_PLAIN
+threshold = 0.80
 
-img_rgb = cv2.imread("./screenshots/full_inventory_copper_top_zoom.jpg")
-# img_rgb = cv2.imread("screenshot.jpg")
-# img_rgb = cv2.imread("./screenshots/screenshot.jpg")
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+try:
+    os.remove("./screenshots/screenshot.png")
+    os.remove("screenshot.png")
+except FileNotFoundError:
+    print()
 
-east_rock_template = cv2.imread('./screenshots/lumbride_east_copper_rock_full.png', 0)
-south_rock_template = cv2.imread('./screenshots/lumbride_south_copper_rock_full.png', 0)
-inventory_ore_template = cv2.imread('./screenshots/copper_ore.png', 0)
+pyautogui.screenshot("./screenshots/screenshot.png")
 
-east_rock_w, east_rock_h = east_rock_template.shape[::-1]
-south_rock_w, south_rock_h = south_rock_template.shape[::-1]
-inventory_ore__w, inventory_ore__h = inventory_ore_template.shape[::-1]
+screenshot_rgb = cv2.imread("./screenshots/screenshot.png")
+screenshot_gray = cv2.cvtColor(screenshot_rgb, cv2.COLOR_BGR2GRAY)
 
-# east_rock_mineable = False
-# south_rock_mineable = False
+east_rock__full_template = cv2.imread('./screenshots/lumbride_east_copper_rock_full.png', 0)
+south_rock__full_template = cv2.imread('./screenshots/lumbride_south_copper_rock_full.png', 0)
 
-east_res = cv2.matchTemplate(img_gray, east_rock_template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.75
-# threshold = 0.90
+east_rock__full_w, east_rock_full_h = east_rock__full_template.shape[::-1]
+south_rock_full_w, south_rock_full_h = south_rock__full_template.shape[::-1]
+
+east_rock_mineable = False
+east_res = cv2.matchTemplate(screenshot_gray, east_rock__full_template, cv2.TM_CCOEFF_NORMED)
 east_loc = np.where( east_res >= threshold)
-if len(east_loc) / 2 == True:
-    east_rock_mineable = True
-
-# print(east_rock_mineable)
-# print(east_res >= 0.9)
-
-# east_rock_mineable = bool(east_loc)
-# print("East Rock Minable = " + str(east_rock_mineable))
-    # print("Right rock can be mined")
 
 for pt in zip(*east_loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + east_rock_w, pt[1] + east_rock_h), (255, 255, 255), 1)
-    # cv2.putText(img_rgb,'Copper Rock', (pt[0] , pt[1]), font, 3, (255,255,255), 1, cv2.LINE_AA)
+    cv2.rectangle(screenshot_rgb, pt, (pt[0] + east_rock__full_w, pt[1] + east_rock_full_h), (255, 255, 255), 1)
 
-south_res = cv2.matchTemplate(img_gray, south_rock_template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.75
-# threshold = 0.9
+south_rock_mineable = False
+south_res = cv2.matchTemplate(screenshot_gray, south_rock__full_template, cv2.TM_CCOEFF_NORMED)
 south_loc = np.where( south_res >= threshold)
 
-# south_rock_mineable = bool(south_loc)
-# print("South Rock Minable = " + str(south_rock_mineable))
-
-# if south_loc == 1:
-#     print("South rock can be mined")
-
 for pt in zip(*south_loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + south_rock_w, pt[1] + south_rock_h), (255, 255, 255), 1)
-    # cv2.putText(img_rgb,'Copper Rock', (pt[0] , pt[1]), font, 3, (255,255,255), 1, cv2.LINE_AA)
+    cv2.rectangle(screenshot_rgb, pt, (pt[0] + south_rock_full_w, pt[1] + south_rock_full_h), (255, 255, 255), 1)
 
-inventory_ore_res = cv2.matchTemplate(img_gray, inventory_ore_template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.75
-# threshold = 0.9
-inventory_ore_loc = np.where( inventory_ore_res >= threshold)
+cv2.imshow("Lumby Mine", imutils.resize(screenshot_rgb, width=750))
+cv2.imshow("Lumby Mine", screenshot_rgb)
 
-for pt in zip(*inventory_ore_loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + inventory_ore__w, pt[1] + inventory_ore__h), (255, 255, 255), 1)
-    # cv2.putText(img_rgb,'Copper Ore', (pt[0] , pt[1]), font, 3, (255,255,255), 1, cv2.LINE_AA)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-cv2.imshow("Lumby Mine", imutils.resize(img_rgb, width=1000))
-# cv2.imshow("Lumby Mine", img_rgb)
+try:
+    os.remove("./screenshots/screenshot.png")
+except FileNotFoundError:
+    print()
+
+def runLoop():
+    try:
+        os.remove("./screenshots/screenshot.png")
+        os.remove("screenshot.jpg)")
+    except FileNotFoundError:
+        print()
+
+    pyautogui.screenshot("./screenshots/screenshot.png")
+
+    # Colour versions of the sample screenshots
+    east_rock_true_south_rock_true_rgb = cv2.imread("./screenshots/east_rock_true_south_rock_true.png")
+    east_rock_true_south_rock_false_rgb = cv2.imread("./screenshots/east_rock_true_south_rock_false.png")
+    east_rock_false_south_rock_true_rgb = cv2.imread("./screenshots/east_rock_false_south_rock_true.png")
+    east_rock_false_south_rock_false_rgb = cv2.imread("./screenshots/east_rock_false_south_rock_false.png")
+    screenshot_rgb = cv2.imread("./screenshots/screenshot.png")
+
+    # Grayscale versions of the coloured screenshots
+    east_rock_true_south_rock_true_gray = cv2.cvtColor(east_rock_true_south_rock_true_rgb, cv2.COLOR_BGR2GRAY)
+    east_rock_true_south_rock_false_gray = cv2.cvtColor(east_rock_true_south_rock_false_rgb, cv2.COLOR_BGR2GRAY)
+    east_rock_false_south_rock_true_gray = cv2.cvtColor(east_rock_false_south_rock_true_rgb, cv2.COLOR_BGR2GRAY)
+    east_rock_false_south_rock_false_gray = cv2.cvtColor(east_rock_false_south_rock_false_rgb, cv2.COLOR_BGR2GRAY)
+    screenshot_gray = cv2.cvtColor(screenshot_rgb, cv2.COLOR_BGR2GRAY)
+
+    # An example picture of the east rock being full and empty
+    east_rock__full_template = cv2.imread('./screenshots/lumbride_east_copper_rock_full.png', 0)
+    east_rock__empty_template = cv2.imread('./screenshots/lumbride_east_copper_rock_empty.png', 0)
+
+    # An example picture of the south rock being full and empty
+    south_rock__full_template = cv2.imread('./screenshots/lumbride_south_copper_rock_full.png', 0)
+    south_rock__empty_template = cv2.imread('./screenshots/lumbride_south_copper_rock_empty.png', 0)
+
+    # An example picture of a full inventory
+    inventory_ore_template = cv2.imread('./screenshots/copper_ore.png', 0)
+
+    # Width and height of the sample image of an east rock
+    # Full
+    east_rock__full_w, east_rock_full_h = east_rock__full_template.shape[::-1]
+    Empty
+    east_rock__empty_w, east_rock_fempty_h = east_rock__empty_template.shape[::-1]
+
+    # Width and height of the sample image of a south rock
+    # Full
+    south_rock_full_w, south_rock_full_h = south_rock__full_template.shape[::-1]
+    Empty
+    south_rock_empty_w, south_rock_empty_h = south_rock__empty_template.shape[::-1]
+
+    # Width and height of the image of the full inventory
+    inventory_ore__w, inventory_ore__h = inventory_ore_template.shape[::-1]
+
+    east_rock_mineable = False
+    east_res = cv2.matchTemplate(screenshot_gray, east_rock__full_template, cv2.TM_CCOEFF_NORMED)
+    east_loc = np.where( east_res >= threshold)
+
+    if len(east_loc) == 2:
+        for pt in zip(*east_loc[::-1]):
+            cv2.rectangle(screenshot_rgb, pt, (pt[0] + east_rock__full_w, pt[1] + east_rock_full_h), (255, 255, 255), 1)
+            # cv2.putText(screenshot_rgb,'Copper Rock', (pt[0] , pt[1]), font, 3, (255,255,255), 1, cv2.LINE_AA)
+        east_rock_mineable = bool(east_loc)
+
+    south_rock_mineable = False
+    south_res = cv2.matchTemplate(screenshot_gray, south_rock__full_template, cv2.TM_CCOEFF_NORMED)
+    south_loc = np.where( south_res >= threshold)
+
+    if len(south_loc) == 2:
+        for pt in zip(*south_loc[::-1]):
+            cv2.rectangle(screenshot_rgb, pt, (pt[0] + south_rock_full_w, pt[1] + south_rock_full_h), (255, 255, 255), 1)
+        south_rock_mineable = bool(south_loc)
+
+    print("East rock is mineable " + str(east_rock_mineable))
+    print("South rock is mineable " + str(south_rock_mineable))
+
+    inventory_ore_res = cv2.matchTemplate(screenshot_gray, inventory_ore_template, cv2.TM_CCOEFF_NORMED)
+    inventory_ore_loc = np.where( east_res >= threshold)
+    for pt in zip(*inventory_ore_loc[::-1]):
+        cv2.rectangle(screenshot_rgb, pt, (pt[0] + inventory_ore__w, pt[1] + inventory_ore__h), (255, 255, 255), 1)
+
+    cv2.imshow("Lumby Mine", imutils.resize(img_rgb, width=1000))
+    cv2.imshow("Lumby Mine", screenshot_rgb)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    sleep(2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
